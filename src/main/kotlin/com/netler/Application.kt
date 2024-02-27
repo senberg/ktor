fceb7fbad8f86a2f03064b0ccda3efbcaa3b0173
Philip Olsson <philip.olsson@expertake.se>
2024-02-27 14:25:59 +0100

    Initial commit

---
+++ src/main/kotlin/com/netler/Application.kt
@@ -0,0 +1,19 @@
+package com.netler
+
+import com.netler.plugins.configureRouting
+import com.netler.plugins.configureSerialization
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+
+    readlnOrNull()
+}
---
+++ src/test/kotlin/com/netler/ApplicationTest.kt
@@ -0,0 +1,36 @@
+package com.netler
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import io.ktor.server.testing.*
+import kotlin.test.Test
+import kotlin.test.assertContains
+import kotlin.test.assertEquals
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Hello World!", response.bodyAsText())
+    }
+
+    @Test
+    fun testNewEndpoint() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/test1")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("html", response.contentType()?.contentSubtype)
+        assertContains(response.bodyAsText(), "Hello From Ktor")
+    }
+}
